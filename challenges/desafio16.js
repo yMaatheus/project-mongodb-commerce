13367b11f551b8546044896ca9cc5af9372ddd74
Matheus <stvgamer74@gmail.com>
2022-09-15 01:18:33 -0300

    feat: :sparkles: desafio 16 create parameter ultimaModificacao in BigMac

---
+++ challenges/desafio16.js
@@ -0,0 +1,9 @@
+db.produtos.update(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "timestamp" } } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);
